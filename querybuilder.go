46450a60cbcbb6264bd58a4e9b3f0e5f69ee1adb
Douglas Dennys <douglasdennys@walkaba.com>
2024-02-02 20:00:50 -0300

    fix

--- querybuilder.go
+++ querybuilder.go
@@ -3,6 +3,8 @@ package querybuilder
 import (
 	"errors"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/url"
 	"regexp"
 	"strconv"
@@ -130,4 +132,194 @@ func parseBracketParams(qs string, o *Options) error {
 		}
 	}
 	return nil
+}
+
+type QueryBuilder struct {
+	collection       string
+	fieldTypes       map[string]string
+	strictValidation bool
+}
+
+func NewQueryBuilder(collection string, strictValidation ...bool) *QueryBuilder {
+	qb := QueryBuilder{
+		collection:       collection,
+		fieldTypes:       map[string]string{},
+		strictValidation: false,
+	}
+	if len(strictValidation) > 0 {
+		qb.strictValidation = strictValidation[0]
+	}
+	return &qb
+}
+
+func (qb QueryBuilder) setPaginationOptions(pagination map[string]int, opts *options.FindOptions) {
+	if limit, ok := pagination["limit"]; ok {
+		opts.SetLimit(int64(limit))
+		if offset, ok := pagination["offset"]; ok {
+			opts.SetSkip(int64(offset))
+		}
+		if skip, ok := pagination["skip"]; ok {
+			opts.SetSkip(int64(skip))
+		}
+	}
+	if size, ok := pagination["size"]; ok {
+		opts.SetLimit(int64(size))
+		if page, ok := pagination["page"]; ok {
+			opts.SetSkip(int64(page * size))
+		}
+	}
+}
+
+func (qb QueryBuilder) setProjectionOptions(fields []string, opts *options.FindOptions) error {
+	if len(fields) > 0 {
+		prj := map[string]int{}
+		for _, field := range fields {
+			val := 1
+			if field[0:1] == "-" {
+				field = field[1:]
+				val = 0
+			}
+			if len(field) > 0 && field[0:1] == "+" {
+				field = field[1:]
+			}
+			if qb.strictValidation {
+				if _, ok := qb.fieldTypes[field]; !ok {
+					// we have a problem
+					return fmt.Errorf("field %s does not exist in collection %s", field, qb.collection)
+				}
+			}
+			prj[field] = val
+		}
+		if len(prj) > 0 {
+			opts.SetProjection(prj)
+		}
+	}
+
+	return nil
+}
+
+func (qb QueryBuilder) setSortOptions(fields []string, opts *options.FindOptions) error {
+	if len(fields) > 0 {
+		sort := map[string]int{}
+		for _, field := range fields {
+			val := 1
+			if field[0:1] == "-" {
+				field = field[1:]
+				val = -1
+			}
+			if field[0:1] == "+" {
+				field = field[1:]
+			}
+			if qb.strictValidation {
+				if _, ok := qb.fieldTypes[field]; !ok {
+					return fmt.Errorf("field %s does not exist in collection %s", field, qb.collection)
+				}
+			}
+			sort[field] = val
+		}
+		opts.SetSort(sort)
+	}
+	return nil
+}
+
+func (qb QueryBuilder) FindOptions(qo Options) (*options.FindOptions, error) {
+	opts := options.Find()
+	qb.setPaginationOptions(qo.Page, opts)
+	if err := qb.setProjectionOptions(qo.Fields, opts); err != nil {
+		return nil, err
+	}
+	if err := qb.setSortOptions(qo.Sort, opts); err != nil {
+		return nil, err
+	}
+	return opts, nil
+}
+
+func (qb QueryBuilder) Filter(opt Options) (bson.D, error) {
+	var filters bson.D
+	for k, v := range opt.Filter {
+		filters = checkFilter(k, v)
+	}
+	return filters, nil
+}
+
+func compareOperator(value string) string {
+	switch value {
+	case "<>":
+		return "$not"
+	case "<=":
+		return "$lte"
+	case ">=":
+		return "$gte"
+	case "!=":
+		return "$ne"
+	case "<":
+		return "$lt"
+	case ">":
+		return "$gt"
+	//case "<=>":
+	//	return "like"
+	default:
+		return "$eq"
+	}
+}
+
+func checkConstraints(values string) string {
+	constraints := []string{"<>", "<=", ">=", "!=", "<", ">", "<=>"}
+	for _, constr := range constraints {
+		result := strings.Split(values, constr)
+		if len(result) > 1 {
+			return compareOperator(values)
+		}
+	}
+	return "$eq"
+}
+
+func checkFilter(field string, values []string) bson.D {
+	key := strings.Split(field, "][")
+	if len(key) == 1 {
+		if len(values) > 1 {
+			check := checkConstraints(values[0])
+			if check == "$lt" || check == "$gt" || check == "$gte" || check == "$lte" {
+				var acc bson.D
+				for _, value := range values {
+					acc = append(acc,
+						bson.E{
+							Key:   check,
+							Value: value,
+						},
+					)
+				}
+				return bson.D{{
+					field,
+					acc,
+				}}
+			}
+
+			var includes []string
+			for _, value := range values {
+				includes = append(includes, value)
+			}
+			return bson.D{{
+				field,
+				bson.D{{
+					"$in", includes,
+				}},
+			}}
+		}
+		return bson.D{{
+			field,
+			bson.D{{
+				checkConstraints(values[0]),
+				values[0],
+			}},
+		}}
+	}
+
+	return bson.D{{
+		key[0], bson.D{{
+			"$elemMatch", bson.D{{
+				key[1], values[0],
+			}},
+		}},
+	}}
 }
