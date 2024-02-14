811a0e91efa6c2989d409d1301ca2a258e20553e
Douglas Dennys <douglasdennys@walkaba.com>
2024-02-13 21:25:44 -0300

    testando novo find

--- querybuilder.go
+++ querybuilder.go
@@ -1,9 +1,11 @@
 package querybuilder
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/url"
 	"regexp"
@@ -320,4 +322,115 @@ func checkFilter(field string, values []string) bson.D {
 			}},
 		}},
 	}}
+}
+
+type Page struct {
+	CurrentPage int64 `json:"currentPage"`
+	PerPage     int64 `json:"perPage"`
+	From        int64 `json:"from"`
+	To          int64 `json:"to"`
+	Total       int64 `json:"total"`
+	LastPage    int64 `json:"lastPage"`
+}
+
+type Link struct {
+	First string `json:"first"`
+	Last  string `json:"last"`
+	Prev  string `json:"prev"`
+	Next  string `json:"next"`
+}
+
+type Meta struct {
+	Page    Page        `json:"page"`
+	Links   Link        `json:"links"`
+	Filters interface{} `json:"filters"`
+}
+
+type OutPagination struct {
+	Data []*interface{} `json:"data"`
+	Meta Meta           `json:"meta"`
+}
+
+type PaginationBuilder struct {
+	collection *mongo.Collection
+	route      string
+}
+
+func NewPaginationBuilder(collection *mongo.Collection, route string) *PaginationBuilder {
+	return &PaginationBuilder{collection, route}
+}
+
+func (c *PaginationBuilder) Find(payload string) (*OutPagination, error) {
+	opt, err := FromQueryString(payload)
+	if err != nil {
+		return nil, errors.New("invalid query string")
+	}
+	queryString := NewQueryBuilder(true)
+	options, err := queryString.FindOptions(opt)
+	if err != nil {
+		return nil, err
+	}
+	var filters bson.D
+	if len(opt.Filter) > 0 {
+		filters, err = queryString.Filter(opt)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		filters = bson.D{}
+	}
+	count, err := c.collection.CountDocuments(context.TODO(), filters)
+	if err != nil {
+		if err.Error() != "document is nil" {
+			return nil, err
+		}
+	}
+	cursor, err := c.collection.Find(context.TODO(), filters, options)
+	if err != nil {
+		if err.Error() != "document is nil" {
+			return nil, err
+		}
+	}
+	var response []*interface{}
+	if err = cursor.All(context.TODO(), &response); err != nil {
+		return nil, err
+	}
+	var result OutPagination
+	page := int64(opt.Page["page"])
+	size := int64(opt.Page["size"])
+	skip := size * (page - 1)
+	if (page + 1) <= count {
+		result.Meta.Page.LastPage = page + 1
+	} else {
+		result.Meta.Page.LastPage = page
+	}
+	result.Data = response
+	result.Meta.Page.CurrentPage = page
+	result.Meta.Page.PerPage = size
+	result.Meta.Page.Total = count
+	result.Meta.Page.From = skip + 1
+	result.Meta.Page.To = skip + size
+	result.Meta.Links.First = c.generateLink(0, size) + c.generateFilters(opt.Filter)
+	result.Meta.Links.Last = c.generateLink(result.Meta.Page.LastPage, size) + c.generateFilters(opt.Filter)
+	result.Meta.Links.Prev = c.generateLink(result.Meta.Page.CurrentPage-1, size) + c.generateFilters(opt.Filter)
+	result.Meta.Links.Next = c.generateLink(result.Meta.Page.CurrentPage+1, size) + c.generateFilters(opt.Filter)
+	result.Meta.Filters = payload
+	return &result, nil
+}
+
+func (c *PaginationBuilder) generateLink(page, size int64) string {
+	return "/" + c.route + "/?page[page]=" + strconv.FormatInt(page, 10) + "&page[size]=" + strconv.FormatInt(size, 10)
+}
+
+func (c *PaginationBuilder) generateFilters(filters map[string][]string) string {
+	var result string
+	for key, value := range filters {
+		isArray := strings.Split(key, "][")
+		if len(isArray) > 1 {
+			result += "&filter[" + isArray[0] + "][" + isArray[1] + "]=" + strings.Join(value, ",")
+		} else {
+			result += "&filter[" + key + "]=" + strings.Join(value, ",")
+		}
+	}
+	return result
 }
